f17a96692d2be2022234d7f842a964aa2f1a29a2
Krish Chandarana <53926201+krishchandarana@users.noreply.github.com>
2024-02-28 21:13:11 -0500

    Form PHP

---
+++ php/contact.php
@@ -0,0 +1,37 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = trim($_POST["subject"]);
+    $message = trim($_POST["message"]);
+
+    // Specify your email and subject
+    $to = "krishchandarana7@gmail.com"; // CHANGE THIS to your email
+    $email_subject = "New contact from $name: $subject";
+
+    // Construct the email body
+    $email_body = "Name: $name\n";
+    $email_body .= "Email: $email\n\n";
+    $email_body .= "Message:\n$message\n";
+
+    // Headers
+    $headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        // Set a 200 (okay) response code.
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        // Set a 500 (internal server error) response code.
+        http_response_code(500);
+        echo "Oops! Something went wrong, and we couldn't send your message.";
+    }
+} else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
